7c11b1311f00870d82a4692ff5080df979a712bb
samrat-chauhan24 <chauhansamrat835@gmail.com>
2026-02-11 12:29:54 +0530

    insert position using lower bound

---
+++ binarysearch1d/SearchInsertPos.java
@@ -0,0 +1,19 @@
+package binarysearch1d;
+
+public class SearchInsertPos {
+    static int searchInsert(int nums[], int target){
+    for(int i = 0; i < nums.length; i++){
+        if(nums[i] >= target){
+            return i;
+        }
+    }
+    return nums.length;
+
+
+}
+    public static void main(String[] args) {
+        int nums[] = {1,3,5,6};
+        int ans = searchInsert(nums, 7);
+        System.out.println(ans);
+    }
+}
