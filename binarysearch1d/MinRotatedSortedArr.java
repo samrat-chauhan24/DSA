fbf4b6b395711b64a3228cb93e2bed3dc4cbc7de
samrat-chauhan24 <chauhansamrat835@gmail.com>
2026-02-12 14:59:09 +0530

    while finding min in rotated array, there's no need to maintain an variable

---
+++ binarysearch1d/MinRotatedSortedArr.java
@@ -0,0 +1,28 @@
+package binarysearch1d;
+
+public class MinRotatedSortedArr {
+    static int findMin(int nums[]){
+        int min = Integer.MAX_VALUE;
+        int l = 0, r = nums.length-1;
+        while (l<=r) {
+            int mid = l + (r-l)/2;
+            if (nums[mid]<min) {
+                min = nums[mid];
+            }
+            if (nums[l]<= nums[mid]) {
+                if (nums[l] <= min && min < nums[mid]) r = mid-1;
+                else l = mid + 1;
+            }
+            else{
+                if(nums[mid] < min && min <= nums[r]) l = mid + 1;
+                else r = mid - 1; 
+            }
+        }
+        return min; 
+    }
+    public static void main(String[] args) {
+        int nums[] = {4,5,6,7,0,1,2,3};
+        int min = findMin(nums);
+        System.out.println(min);
+    }
+}
