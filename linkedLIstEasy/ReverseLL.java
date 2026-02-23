adb86da5c8411434cea472b4861bfc8ee4087a4e
samrat-chauhan24 <chauhansamrat835@gmail.com>
2026-02-23 12:15:28 +0530

    reversing is LL is a bit easy and trick too. beacuse its hard to visualize.

---
+++ linkedLIstEasy/ReverseLL.java
@@ -0,0 +1,51 @@
+package linkedLIstEasy;
+import java.lang.classfile.constantpool.IntegerEntry;
+import java.util.Stack;
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int val){ 
+        this.val = val;
+    }
+    ListNode(int val, ListNode next) { 
+        this.val = val; this.next = next;
+    }
+}
+public class ReverseLL {
+    static ListNode reverseListOpti(ListNode head){
+        ListNode currNode = head;
+        ListNode prevNode = null;
+        while(currNode!=null){
+            ListNode nextOfCurrNode = currNode.next;
+            nextOfCurrNode.next = prevNode;
+            currNode = nextOfCurrNode;
+            prevNode = currNode;
+        }
+        return head;
+    }
+    static ListNode reverseListBF(ListNode head){
+        Stack<Integer> stack = new Stack<>();
+        ListNode temp = head;
+        while (temp!=null) {
+            stack.push(temp.val);
+            temp = temp.next;
+        }
+        temp = head;
+        while (temp!=null) {
+            temp.val = stack.pop();
+            temp = temp.next;
+        }
+        return head;
+    }
+    static ListNode conversionToArr(int arr[]){
+        if(arr.length==0) return null;
+        ListNode head = new ListNode(arr[0]);
+        ListNode mover = head;
+        for(int i  =0; i<arr.length; i++){
+            ListNode temp = new ListNode(arr[i]);
+            mover.next = temp;
+            mover = temp;
+        }
+        return head;
+    }
+}
